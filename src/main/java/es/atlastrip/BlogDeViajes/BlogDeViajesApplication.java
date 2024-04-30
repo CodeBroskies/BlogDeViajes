5ca16fca5d99db92b5e2d43396a432d230f6d099
Michillas <elrickimont@gmail.com>
2024-04-30 11:50:10 +0200

    BlogDeViajes - First commit

---
+++ src/main/java/es/atlastrip/BlogDeViajes/BlogDeViajesApplication.java
@@ -0,0 +1,13 @@
+package es.atlastrip.BlogDeViajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDeViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDeViajesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/atlastrip/BlogDeViajes/BlogDeViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.atlastrip.BlogDeViajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDeViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
