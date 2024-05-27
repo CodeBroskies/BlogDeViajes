eefc1b9a7b2f51d095f011890d5e0bddf5e3e26d
Michillas <elrickimont@gmail.com>
2024-05-27 11:09:11 +0200

    BlogDeViajes M - Code corrections

--- src/main/java/es/atlastrip/BlogDeViajes/controllers/TipoController.java
+++ src/main/java/es/atlastrip/BlogDeViajes/controllers/TipoController.java
@@ -8,51 +8,47 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+
+@Controller
+@RequestMapping("/tipos")
 public class TipoController {
 
+    TipoService service = new TipoService();
 
-    @Controller
-    @RequestMapping("/tipos")
-    public class ControladorTipo {
+    @GetMapping("/listar")
+    public String listar(@RequestParam Model model) throws SQLException {
+        String busqueda = "";
+        ArrayList<Tipo> tipos = service.listarTipos();
+        model.addAttribute("tipos", service.listarTipos());
+        model.addAttribute("tipo", new Tipo());
+        model.addAttribute("busqueda", busqueda);
+        return "tipos";
+    }
 
-        TipoService service = new TipoService();
+    @GetMapping("/eliminar")
+    public String eliminar(@RequestParam("id") int id, Model model) throws SQLException {
+        service.eliminarTipo(id);
+        model.addAttribute("tipos", service.listarTipos());
+        return "redirect:/tipos/listar?pagina=1";
+    }
 
-        @GetMapping("/listar")
-        public String listar(@RequestParam Model model) throws SQLException {
-            String busqueda = "";
-            ArrayList<Tipo> tipos = service.listarTipos();
-            model.addAttribute("tipos", service.listarTipos());
-            model.addAttribute("tipo", new Tipo());
-            model.addAttribute("busqueda", busqueda);
-            return "tipos";
-        }
+    @GetMapping("/modificar")
+    public String modificar(@RequestParam("id") int id, Model model) throws SQLException {
+        model.addAttribute("tipoSeleccionado", service.obtenerTipo(id));
+        return "tipoModificar";
+    }
 
-        @GetMapping("/eliminar")
-        public String eliminar(@RequestParam("id") int id, Model model) throws SQLException {
-            service.eliminarTipo(id);
-            model.addAttribute("tipos", service.listarTipos());
-            return "redirect:/tipos/listar?pagina=1";
-        }
+    @PostMapping("/actualizar")
+    public String actualizar(@ModelAttribute Tipo tipo, Model model) throws SQLException {
+        service.actualizarTipo(tipo);
+        model.addAttribute("tipos", service.listarTipos());
+        return "redirect:/tipos/listar?pagina=1";
+    }
 
-        @GetMapping("/modificar")
-        public String modificar(@RequestParam("id") int id, Model model) throws SQLException {
-            model.addAttribute("tipoSeleccionado", service.obtenerTipo(id));
-            return "tipoModificar";
-        }
-
-        @PostMapping("/actualizar")
-        public String actualizar(@ModelAttribute Tipo tipo, Model model) throws SQLException {
-            service.actualizarTipo(tipo);
-            model.addAttribute("tipos", service.listarTipos());
-            return "redirect:/tipos/listar?pagina=1";
-        }
-
-        @PostMapping("/agregar")
-        public String agregar(@ModelAttribute Tipo tipo, Model model) throws SQLException {
-            service.crearTipo(tipo);
-            model.addAttribute("tipos", service.listarTipos());
-            return "redirect:/tipos/listar?pagina=1";
-        }
-
+    @PostMapping("/agregar")
+    public String agregar(@ModelAttribute Tipo tipo, Model model) throws SQLException {
+        service.crearTipo(tipo);
+        model.addAttribute("tipos", service.listarTipos());
+        return "redirect:/tipos/listar?pagina=1";
     }
 }
